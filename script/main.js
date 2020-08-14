4aaad25819e3ffecc26497678acf49642321037d
xiaoliangyu <940044597@qq.com>
2020-08-14 08:54:21 +0800

    111

---
+++ script/main.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+"use strict";_require.config({paths:{jquery:"https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery",jquerycookie:"https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.4.1/jquery.cookie",jquerylazyload:"https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload"}}),_require(["jquery"],function(e){var t=e("#currentpage").attr("targetpage");t&&_require([t],function(e){e.init()})});
+=======
+"use strict";_require.config({paths:{jquery:"https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery",jquerycookie:"https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.4.1/jquery.cookie",jquerylazyload:"https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload"},shim:{deps:["jquery"]}}),_require(["jquery"],function(e){var r=e("#currentpage").attr("targetpage");r&&_require([r],function(e){e.init()})});
+>>>>>>> chengpin
